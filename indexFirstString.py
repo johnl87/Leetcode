ac9f23a981736785b87cea76404a62f45b46c90f
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-15 08:46:05 -0400

    lc 28

---
+++ indexFirstString.py
@@ -0,0 +1,8 @@
+# LC 28
+class Solution:
+    def strStr(haystack: str, needle: str) -> int:
+        word = needle
+        print (word)
+        return haystack.find((needle))
+    
+    print(strStr("sadbutsad", "sad"))
