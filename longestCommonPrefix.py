20c9bdc59e993c6b7784b90cadbe5e384a3ff7f1
John Le <65139320+johnl87@users.noreply.github.com>
2023-03-16 13:45:38 -0400

    lc 14

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,3 +1,15 @@
 class Solution:
-    def longestCommonPrefix(strs: List[str]) -> str:
-        
+    def longestCommonPrefix(strs: list[str]) -> str:
+        result = ""
+
+        prefix = strs[0]
+        #print(prefix)
+
+        for item in strs[1:]:
+            while item[:len(prefix)] != prefix and prefix:
+                prefix = prefix[:len(prefix) -1]
+            if not prefix:
+                break
+        result = prefix
+        return result
+       
