89fc1cf6ad6034d306f64414bc49b5b467fe0755
John Le <65139320+johnl87@users.noreply.github.com>
2023-05-08 17:06:28 -0400

    comments added for lc 217

--- containsDupe.py
+++ containsDupe.py
@@ -1,7 +1,13 @@
 # lc 217
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # create an empty set
         numbers = set()
+        # run through the given nums array, if the number is not already inside set
+        # then add the number to the set, otherwise return True because the second
+        # occurance will confirm that the nums array has more than one of the 
+        # same numbers
+        
         for number in nums:
             if number not in numbers:
                 numbers.add(number)
